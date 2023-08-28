18c2a1913d34ae5b588cbe93c246769cd9d13770
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-28 10:45:58 -0300

    Instalando Dropzone + Componente

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useContext } from "react";
 import Layout from "../components/Layout";
 import authContext from "../context/auth/authContext";
+import Link from "next/link";
+import Dropzone from "../components/Dropzone";
 
 export default function Home() {
   // Extraer el usuario autenticado del storage
@@ -13,7 +15,28 @@ export default function Home() {
 
   return (
     <Layout>
-      <h1>Index</h1>
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+        <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
+          <Dropzone />
+          <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
+            <p className="text-4xl font-bold font-sans text-gray-800">
+              Compartir archivos de forma sencilla y privada
+            </p>
+            <p className="text-lg mt-5 leading-loose">
+              <span className="text-red-500 font-bold">ReactNodeSend</span> te
+              permite compartir archivos con cifrado de extremo a extremo y un
+              archivo que es eliminado despues de ser descargado. Asi que puedes
+              mantener lo que compartes en privado y asegurarte de que tus cosas
+              no permanezcan en linea para siempre.
+            </p>
+            <Link href="/crearcuenta" legacyBehavior>
+              <a className="text-red-500 font-bold text-lg hover:text-red-700">
+                Crea una cuenta para mayores beneficios
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
