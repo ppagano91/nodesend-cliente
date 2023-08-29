1c45f203f0e0598cabb2d0db5bc09c4501706474
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-29 18:38:15 -0300

    Mostrando un Formulario para Passwords

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Formulario = () => {
+  const [tienePassword, setTienePassword] = useState(false);
+
   return (
     <div className="w-full mt-20">
-      <label className="text-lg text-gray-800">Eliminar después de:</label>
-      <select className="appearance-none w-full mt-2 bg-white border border-gray-400 text-black py-3 px-4 pr-8 rounded leading-none focus:outline-none focus:border-gray-500">
-        <option value="" selected disabled>
-          -- Seleccione --
-        </option>
-        <option value="1">1 Descarga</option>
-        <option value="5">5 Descargas</option>
-        <option value="10">10 Descargas</option>
-        <option value="20">20 Descargas</option>
-      </select>
+      <div>
+        <label className="text-lg text-gray-800">Eliminar después de:</label>
+        <select className="appearance-none w-full mt-2 bg-white border border-gray-400 text-black py-3 px-4 pr-8 rounded leading-none focus:outline-none focus:border-gray-500">
+          <option value="" selected disabled>
+            -- Seleccione --
+          </option>
+          <option value="1">1 Descarga</option>
+          <option value="5">5 Descargas</option>
+          <option value="10">10 Descargas</option>
+          <option value="20">20 Descargas</option>
+        </select>
+      </div>
+      <div className="mt-4">
+        <div className="flex justify-between items-center">
+          <label className="text-lg text-gray-800 mr-2">
+            Proteger con contraseña
+          </label>
+          <input
+            type="checkbox"
+            className="mr-2 mt-1"
+            onChange={() => setTienePassword(!tienePassword)}
+          />
+        </div>
+        {tienePassword ? (
+          <input
+            type="password"
+            className="appearance-none w-full mt-2 bg-white border border-gray-400 text-black py-3 px-4 pr-8 rounded leading-none focus:outline-none focus:border-gray-500"
+            placeholder="Escriba la contraseña"
+          />
+        ) : null}
+      </div>
     </div>
   );
 };
